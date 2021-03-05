993557809a171c57596ac4d5d84b5cc8711cad67
Akhil Jose <77378123+akhiljoseko@users.noreply.github.com>
2021-03-05 10:29:13 +0530

    Update appbar.dart

--- lib/goldWidgets/appbar.dart
+++ lib/goldWidgets/appbar.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:foodieshop/goldPages/login.dart';
 import 'package:foodieshop/goldWidgets/goldSetting.dart';
 
-
 class FoodieAppbar extends StatefulWidget implements PreferredSizeWidget {
   final PreferredSizeWidget bottom;
   final double preferredHeight;
- 
+
   final String title;
-  FoodieAppbar({Key key, this.title, this.bottom, this.preferredHeight=60.0})
+  FoodieAppbar({Key key, this.title, this.bottom, this.preferredHeight = 60.0})
       : preferredSize = Size.fromHeight(preferredHeight),
         super(key: key);
 
@@ -21,47 +20,48 @@ class FoodieAppbar extends StatefulWidget implements PreferredSizeWidget {
 
 class _FoodieAppbarState extends State<FoodieAppbar> {
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return AppBar(
-       bottom: widget.bottom,
-       elevation: 0,
-       shadowColor: themecolor,
-       backgroundColor: themecolor,
-        title:Image.asset('images/logo.png',width: 100,height: 100,),
-        actions: [
-          Container(
-            padding: EdgeInsets.only(top:20),
-            child: Text("SPOON",style: TextStyle(fontSize:18),)),
-            Container(
-            padding: EdgeInsets.only(top:5),
-            child:PopupMenuButton<String>(
-              icon: Image.asset('images/arrowdown.png',),
-            
-              itemBuilder: (context) => [
-  
-  
-        PopupMenuItem(
-           
-            child: Row(
-          children: <Widget>[
-            GestureDetector(
-                onTap:(){
-              Navigator.push(context, MaterialPageRoute(builder: (context){
-                return Login();
-              }));
-            },
-              child: Text('Log Out',style: TextStyle(fontSize:18)))
-          ],
-        )),
-      ])
-
-
+      bottom: widget.bottom,
+      elevation: 0,
+      shadowColor: themecolor,
+      backgroundColor: themecolor,
+      title: Image.asset(
+        'images/logo.png',
+        width: 100,
+        height: 100,
+      ),
+      actions: [
+        Container(
+            padding: EdgeInsets.only(top: 20),
+            child: Text(
+              "SPOON",
+              style: TextStyle(fontSize: 18),
+            )),
+        Container(
+          padding: EdgeInsets.only(top: 5),
+          child: PopupMenuButton<String>(
+            icon: Image.asset(
+              'images/arrowdown.png',
             ),
-            
-                ],
-                );
-  
-  
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                value: 'logout',
+                child: Text("Log Out"),
+              ),
+            ],
+            onSelected: (value) {
+              switch (value) {
+                case 'logout':
+                  Navigator.of(context).pushAndRemoveUntil(
+                      MaterialPageRoute(builder: (context) => Login()),
+                      (Route<dynamic> route) => false);
+                  break;
+              }
+            },
+          ),
+        ),
+      ],
+    );
   }
-  
 }
